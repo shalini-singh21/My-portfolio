bd3c9e3427ac14c7d3cd4446080e892266bd04d1
shalini singh <shalu.singh13042gmail.com>
2024-12-29 00:23:14 +0530

    removing some text padding

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -6,22 +6,22 @@ const Intro = () => {
     return (
 
         <>
-            
-               
-                <section id="intro">
-                    <div className="introContent">
-                        <span className="hello"> Hello,</span> <br></br>
-                        <span className="introText"> I'm<span className="introName"> Shalini singh</span> <br></br>Web Developer</span>
-                        <p className="introParagh">  Developed proficiency in <strong> Frontend Development</strong> by implementing solutions in <strong>ReactJS, Java,SQL</strong> .
-                         to enhance user experiences. <br></br> Targeting roles as a <strong> Frontend Development.</strong>  </p>
-                        {/* <a><button className="btn"><img src="" alt="" />Hire me</button></a> */}
-                    </div>
-                <img src={shalu} alt="shalu" className="shalu"/>
-                 
-                </section>
 
-              
-            
+
+            <section id="intro">
+                <div className="introContent">
+                    <span className="hello"> Hello,</span> <br></br>
+                    <span className="introText"> I'm<span className="introName"> Shalini singh</span> <br></br>Web Developer</span>
+                    <p className="introParagh">  Developed proficiency in <strong> Frontend Development</strong> by implementing solutions in <strong>ReactJS, Java,SQL</strong> .
+                        <br></br>to enhance user experiences. <br></br> Targeting roles as a <strong> Frontend Development.</strong>  </p>
+                    {/* <a><button className="btn"><img src="" alt="" />Hire me</button></a> */}
+                </div>
+                <img src={shalu} alt="shalu" className="shalu" />
+
+            </section>
+
+
+
         </>
     )
 }
