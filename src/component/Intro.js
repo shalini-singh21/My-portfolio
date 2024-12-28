77a3207aa3611bb7ecab3c851d18ef19bd4f7f47
shalini singh <shalu.singh13042gmail.com>
2024-12-29 00:41:11 +0530

    hii

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -14,7 +14,7 @@ const Intro = () => {
                     <span className="introText"> I'm<span className="introName"> Shalini singh</span> <br></br>Web Developer</span>
                     <p className="introParagh">  Developed proficiency in <strong> Frontend Development</strong> by implementing solutions in <strong>ReactJS, Java,SQL</strong> .
                         <br></br>to enhance user experiences. <br></br> Targeting roles as a <strong> Frontend Development.</strong>  </p>
-                    {/* <a><button className="btn"><img src="" alt="" />Hire me</button></a> */}
+                  
                 </div>
                 <img src={shalu} alt="shalu" className="shalu" />
 
