cc3a5034a809584c38c4948ac033d9636f108f1b
shalini singh <shalu.singh13042gmail.com>
2024-12-28 23:54:18 +0530

    hiiiii

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -13,7 +13,7 @@ const Intro = () => {
                         <span className="hello"> Hello,</span> <br></br>
                         <span className="introText"> I'm<span className="introName"> Shalini singh</span> <br></br>Web Developer</span>
                         <p className="introParagh">  Developed proficiency in <strong> Frontend Development</strong> by implementing solutions in <strong>ReactJS, Java,SQL</strong> .
-                         to enhance user experiences. Targeting roles as a <strong> Frontend Development.</strong>  </p>
+                         to enhance user experiences. <br></br> Targeting roles as a <strong> Frontend Development.</strong>  </p>
                         {/* <a><button className="btn"><img src="" alt="" />Hire me</button></a> */}
                     </div>
                 <img src={shalu} alt="shalu" className="shalu"/>
